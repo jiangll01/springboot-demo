3ba27f1fe31e765547ff59f465949121fa1e1443
jiangll <jiangll01@inspur.com>
2020-06-12 09:06:10 +0800

    springboot学习

---
+++ src/main/java/com/dream/order/SpringbootDemo.java
@@ -0,0 +1,20 @@
+package com.dream.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author jiangll01
+ * @Date: 2020/6/11 10:21
+ * @Description:
+ */
+@SpringBootApplication
+
+public class SpringbootDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo.class,args);
+    }
+}
